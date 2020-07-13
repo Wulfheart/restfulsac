50c8c8fe2654aa0a563b17bdce178ce071c66325
Alexander Wulf <alexander.wulf@kefischer.de>
2020-07-13 14:38:51 +0200

    Dependencies

--- src/Commands/SkeletonCommand.php
+++ src/Commands/SkeletonCommand.php
@@ -4,11 +4,14 @@ namespace Spatie\Skeleton\Commands;
 
 use Illuminate\Console\Command;
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
+
 class SkeletonCommand extends Command
 {
     public $signature = 'make:restfulsac {path}';
 
-    public $description = 'Creates a folder of restful single action controllers';
+    public $description = 'Create a folder of restful single action controllers';
 
     public function handle()
     {
@@ -16,28 +19,28 @@ class SkeletonCommand extends Command
         $methodsPlural = ['index'];
         $controllers = collect();
         $pathArray = collect(explode('/', $this->argument('path')));
-        $pathArray->transform(function ($item, $key) {
+        $pathArray->transform(function ($item, $key){
             return Str::studly($item);
         });
         $last = $pathArray->last();
         foreach ($methodsSingular as $ms) {
             $pathArray->add(Str::studly(join(" ", [$ms, Str::singular($last),
-                'Controller', ])));
+                'Controller'])));
             $controllerName = $pathArray->join('/');
             $this->info($controllerName);
             $controllers->push($controllerName);
             $pathArray->pop();
         }
-        foreach ($methodsPlural as $mp) {
+        foreach ($methodsPlural as $mp){
             $pathArray->add(Str::studly(join(" ", [$mp, Str::plural($last),
-                'Controller', ])));
+                'Controller'])));
             $controllerName = $pathArray->join('/');
             $this->info($controllerName);
             $controllers->push($controllerName);
             $pathArray->pop();
         }
 
-        foreach ($controllers as $cn) {
+        foreach ($controllers as $cn){
             $this->warn($cn);
             $this->call("make:controller", ['-i' => true, 'name' => $cn]);
         }
