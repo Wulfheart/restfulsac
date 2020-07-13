2e2923da81c09a077d53a3fcd621d275a8211d94
Alexander Wulf <alexander.wulf@kefischer.de>
2020-07-13 14:25:53 +0200

    Command scaffolded

--- src/Commands/SkeletonCommand.php
+++ src/Commands/SkeletonCommand.php
@@ -6,12 +6,40 @@ use Illuminate\Console\Command;
 
 class SkeletonCommand extends Command
 {
-    public $signature = 'skeleton';
+    public $signature = 'make:restfulsac {path}';
 
-    public $description = 'My command';
+    public $description = 'Creates a folder of restful single action controllers';
 
     public function handle()
     {
-        $this->comment('All done');
+        $methodsSingular = ['create', 'store', 'show', 'update', 'destroy'];
+        $methodsPlural = ['index'];
+        $controllers = collect();
+        $pathArray = collect(explode('/', $this->argument('path')));
+        $pathArray->transform(function ($item, $key){
+            return Str::studly($item);
+        });
+        $last = $pathArray->last();
+        foreach ($methodsSingular as $ms) {
+            $pathArray->add(Str::studly(join(" ", [$ms, Str::singular($last),
+                'Controller'])));
+            $controllerName = $pathArray->join('/');
+            $this->info($controllerName);
+            $controllers->push($controllerName);
+            $pathArray->pop();
+        }
+        foreach ($methodsPlural as $mp){
+            $pathArray->add(Str::studly(join(" ", [$mp, Str::plural($last),
+                'Controller'])));
+            $controllerName = $pathArray->join('/');
+            $this->info($controllerName);
+            $controllers->push($controllerName);
+            $pathArray->pop();
+        }
+
+        foreach ($controllers as $cn){
+            $this->warn($cn);
+            $this->call("make:controller", ['-i' => true, 'name' => $cn]);
+        }
     }
 }
